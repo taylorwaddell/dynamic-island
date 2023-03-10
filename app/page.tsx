22727d7cf77ff24f8bb9bd440dbe3c4b65066abc
Taylor Waddell <74875060+taylorwaddell@users.noreply.github.com>
2023-03-10 11:07:46 -0600

    Fixed animations and added ⌘K

--- app/page.tsx
+++ app/page.tsx
@@ -1,93 +1,99 @@
 "use client"; // this is a client component 👈🏽
 
 import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 import CodeSVG from "@/public/JSX-icons/CodeSVG";
 import HomeSVG from "@/public/JSX-icons/HomeSVG";
 import InfoSVG from "@/public/JSX-icons/InfoSVG";
 import SearchSVG from "@/public/JSX-icons/SearchSVG";
 import styles from "./page.module.scss";
-import { useState } from "react";
 
 export default function Home() {
-  const [x, setX] = useState(0);
   const [isVisible, setIsVisible] = useState(false);
 
   function handleSearchClick(): void {
     setIsVisible(() => !isVisible);
-    if (x === 0) setX(() => -81);
-    if (x === -81) setX(() => 0);
   }
-  return (
-    <AnimatePresence>
-      <motion.div
-        animate={{ width: "300px" }}
-        transition={{ type: "spring", stiffness: 100 }}
-        className={styles.bar}
-      >
-        {!isVisible && (
-          <>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className={styles.iconWrapper}
-            >
-              <HomeSVG classes={styles.icons} />
-            </motion.div>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className={styles.searchIconWrapper}
-              onClick={() => handleSearchClick()}
-            >
-              <SearchSVG classes={styles.icons} />
-            </motion.div>
-          </>
-        )}
 
-        {isVisible && (
-          <>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className={styles.searchIconWrapper}
-              onClick={() => handleSearchClick()}
-            >
-              <SearchSVG classes={styles.icons} />
-            </motion.div>
-            <motion.input
-              autoFocus
-              className={styles.searchBar}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            />
-          </>
-        )}
-        {!isVisible && (
-          <>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className={styles.iconWrapper}
-            >
-              <CodeSVG classes={styles.icons} />
-            </motion.div>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className={styles.iconWrapper}
-            >
-              <InfoSVG classes={styles.icons} />
-            </motion.div>
-          </>
-        )}
+  const handleKeyPress = (e: KeyboardEvent) => {
+    if (e.key === 'k' && e.metaKey) handleSearchClick();
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  });
+
+  return (
+    <motion.div
+      animate={{ width: `${!isVisible ? "300px" : "400px"}` }}
+      transition={{ type: "spring", stiffness: 100 }}
+      layout
+      className={styles.bar}
+    >
+      {!isVisible && (
+        <AnimatePresence>
+          <motion.div
+            key={"home"}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={styles.iconWrapper}
+          >
+            <HomeSVG classes={styles.icons} />
+          </motion.div>
+        </AnimatePresence>
+      )}
+      {!isVisible && (
+        <AnimatePresence>
+          <motion.div
+            key={"code"}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={styles.iconWrapper}
+          >
+            <CodeSVG classes={styles.icons} />
+          </motion.div>
+          <motion.div
+            key={"info"}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={styles.iconWrapper}
+          >
+            <InfoSVG classes={styles.icons} />
+          </motion.div>
+        </AnimatePresence>
+      )}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        key={"search"}
+        className={`${styles.searchIconWrapper} ${isVisible && styles.gray} ${
+          isVisible && styles.borderRadiusAdjustment
+        } ${!isVisible && styles.grayHover}`}
+        onClick={() => handleSearchClick()}
+        layout
+      >
+        <SearchSVG classes={styles.icons} />
       </motion.div>
-    </AnimatePresence>
+
+      {isVisible && (
+        <motion.input
+          autoFocus
+          key={"searchInput"}
+          className={`${styles.searchBar} ${isVisible && styles.gray}`}
+          initial={{ width: "0px" }}
+          animate={{ width: "100%" }}
+          transition={{ duration: .325 }}
+          exit={{ width: "0px" }}
+        />
+      )}
+    </motion.div>
   );
 }
